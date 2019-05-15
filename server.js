2b517ead07ebb3828b5a4ff98fe474aaad71482c
Marcelo Laos <mlaos1997@gmail.com>
2019-05-14 23:11:53 -0400

    need to rethink my approach... possibly restarting

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const usersRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
 
 const server = express();
-
 server.use(express.json());
 
 // Custom middleware
 server.use(logger);
 
 server.use('/api/users', usersRouter);
+server.use('/api/post', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -20,7 +21,7 @@ function logger(req, res, next) {
   console.log(`Request Type: ${req.method}`);
   console.log(`Request Url: ${req.url}`);
   console.log(`Request Url: ${req.url}`);
-  console.log(Date.now());
+  console.log(new Date().getTime());
   next();
 };
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -3,46 +3,85 @@ const userDb = require('./userDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validatePost, validateUser, async (req, res) => {
     const { name } = req.body;
+    userDb.insert(name)
+    .then(res => {
+        res.json(res);
+    })
+    .catch(err => {
+        res.status(500).json({ err: 'Database could not get info'});
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-
-});
-
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const user = await userDb.get();
+        res.json(user);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
-
+    res.json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const posts = await userDb.getUserPosts(id)
+        if(posts) {
+            res.status(200).json(posts);
+        } else {
+            res.status(400).json({ message: 'Could not get posts'});
+        }
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await userDb.remove(id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ message: 'User cannot be removed'});
+    }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateUserId, validateUser, async (req, res) => {
+    try {
+        
+    }
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+    try {
+        const { id } = req.params;
+        const user = await userDb.getById(id);
+        if (user) {
+            req.user = user;
+            next();
+        } else {
+            res.status(404).json({ message: 'invalid user id' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Failed to process request'});
+    }
+    next();
 };
 
-function validateUser(req, res, next) {
-
+async function validateUser(req, res, next) {
+    const { name } = req.body;
+    if(!name) {
+        res.status(400).json({ message: "Missing required name field"})
+    } else if (!req.body) {
+        res.status(400).json({message: 'missing user data'})
+    }
+    next();
 };
 
-function validatePost(req, res, next) {
-
-};
 
 module.exports = router;
